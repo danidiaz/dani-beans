13ca1fcc75c19cf06c7677bb60a3feb161b40b70
Daniel Díaz <diaz_carrete@yahoo.com>
2026-05-24 21:49:49 +0200

    lib-sqlite

---
+++ lib-sqlite/Sqlite/Bean.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | Allocating connections that might me used down the hierarchy of calls.
+module Sqlite.Bean
+  ( withConnection,
+    hoistWithConnection,
+    SqlitePoolConfig (..),
+    SqlitePool,
+    makeSqlitePool,
+  )
+where
+
+import Data.Aeson
+import Data.Pool.Introspection qualified
+import Data.Pool.Introspection.Bean
+import Data.Text
+import GHC.Generics (Generic)
+import Sqlite
+import Sqlite.Query (execute_)
+import ThreadLocal
+
+type SqlitePool = Pool Connection
+
+data SqlitePoolConfig = SqlitePoolConfig
+  {databaseFile :: Text}
+  deriving stock (Generic)
+  deriving anyclass (FromJSON, ToJSON)
+
+makeSqlitePool :: SqlitePoolConfig -> PoolConfig -> forall x. (SqlitePool -> IO x) -> IO x
+makeSqlitePool
+  SqlitePoolConfig {databaseFile}
+  poolConf =
+    do
+      Data.Pool.Introspection.Bean.make (Sqlite.openV2NoMutexReadWrite databaseFile) Sqlite.close poolConf
+
+-- | Takes connection from the pool and sets up transactionality.
+withConnection ::
+  SqlitePool ->
+  ThreadLocal Connection ->
+  (forall x. IO x -> IO x)
+withConnection pool threadLocalConnection action =
+  Data.Pool.Introspection.withResource pool \Resource {resource} -> do
+    execute_ resource "BEGIN IMMEDIATE"
+    r <- withThreadLocal threadLocalConnection resource action
+    execute_ resource "COMMIT"
+    pure r
+
+-- | Like 'withConnection', but more in the shape of a decorator.
+hoistWithConnection ::
+  forall bean.
+  ((forall x. IO x -> IO x) -> bean -> bean) ->
+  SqlitePool ->
+  ThreadLocal Connection ->
+  bean ->
+  bean
+hoistWithConnection hoistBean pool tlocal =
+  hoistBean (withConnection pool tlocal)
